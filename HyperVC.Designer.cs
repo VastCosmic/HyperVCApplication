9473db3e66a156d4616e00b107ce1c1bdc058cc1
VastCosmic <137017677@qq.com>
2023-11-07 20:37:11 +0800

    design left

--- HyperVC.Designer.cs
+++ HyperVC.Designer.cs
@@ -29,34 +29,178 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HyperVC));
-            panel1 = new Panel();
+            left = new Panel();
+            left_main4 = new Panel();
+            img_setting = new PictureBox();
+            btn_setting = new Button();
+            left_server = new Panel();
+            img_server = new PictureBox();
+            btn_server = new Button();
+            left_data = new Panel();
+            pictureBox1 = new PictureBox();
+            btn_data = new Button();
+            left_main1 = new Panel();
+            img_home = new PictureBox();
+            btn_home = new Button();
+            left_up = new Panel();
+            img_title = new PictureBox();
             panel2 = new Panel();
-            panel3 = new Panel();
+            panel_background = new Panel();
+            main_up = new Panel();
+            lbl_title = new Label();
             panel4 = new Panel();
             btnClose = new Button();
+            left.SuspendLayout();
+            left_main4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)img_setting).BeginInit();
+            left_server.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)img_server).BeginInit();
+            left_data.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            left_main1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)img_home).BeginInit();
+            left_up.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)img_title).BeginInit();
             panel2.SuspendLayout();
-            panel3.SuspendLayout();
+            main_up.SuspendLayout();
             panel4.SuspendLayout();
-            SuspendLayout();
             // 
-            // panel1
+            // left
             // 
-            panel1.BackColor = Color.FromArgb(116, 166, 168);
-            resources.ApplyResources(panel1, "panel1");
-            panel1.Name = "panel1";
+            left.BackColor = Color.FromArgb(116, 166, 168);
+            left.Controls.Add(left_main4);
+            left.Controls.Add(left_server);
+            left.Controls.Add(left_data);
+            left.Controls.Add(left_main1);
+            left.Controls.Add(left_up);
+            resources.ApplyResources(left, "left");
+            left.Name = "left";
+            // 
+            // left_main4
+            // 
+            left_main4.Controls.Add(img_setting);
+            left_main4.Controls.Add(btn_setting);
+            resources.ApplyResources(left_main4, "left_main4");
+            left_main4.Name = "left_main4";
+            // 
+            // img_setting
+            // 
+            img_setting.BackgroundImage = Properties.Resources._3044137_optimize_options_setting_settings_icon;
+            resources.ApplyResources(img_setting, "img_setting");
+            img_setting.Name = "img_setting";
+            img_setting.TabStop = false;
+            // 
+            // btn_setting
+            // 
+            resources.ApplyResources(btn_setting, "btn_setting");
+            btn_setting.FlatAppearance.BorderSize = 0;
+            btn_setting.Name = "btn_setting";
+            btn_setting.UseVisualStyleBackColor = true;
+            btn_setting.Click += btn_setting_Click;
+            // 
+            // left_server
+            // 
+            left_server.Controls.Add(img_server);
+            left_server.Controls.Add(btn_server);
+            resources.ApplyResources(left_server, "left_server");
+            left_server.Name = "left_server";
+            // 
+            // img_server
+            // 
+            img_server.BackgroundImage = Properties.Resources._1530369_weather_cloud_clouds_cloudy_icon;
+            resources.ApplyResources(img_server, "img_server");
+            img_server.Name = "img_server";
+            img_server.TabStop = false;
+            // 
+            // btn_server
+            // 
+            resources.ApplyResources(btn_server, "btn_server");
+            btn_server.FlatAppearance.BorderSize = 0;
+            btn_server.Name = "btn_server";
+            btn_server.UseVisualStyleBackColor = true;
+            btn_server.Click += btn_server_Click;
+            // 
+            // left_data
+            // 
+            left_data.Controls.Add(pictureBox1);
+            left_data.Controls.Add(btn_data);
+            resources.ApplyResources(left_data, "left_data");
+            left_data.Name = "left_data";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources._3044135_board_layer_overlay_paper_rectangle_icon;
+            resources.ApplyResources(pictureBox1, "pictureBox1");
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.TabStop = false;
+            // 
+            // btn_data
+            // 
+            resources.ApplyResources(btn_data, "btn_data");
+            btn_data.FlatAppearance.BorderSize = 0;
+            btn_data.Name = "btn_data";
+            btn_data.UseVisualStyleBackColor = true;
+            btn_data.Click += btn_data_Click;
+            // 
+            // left_main1
+            // 
+            left_main1.Controls.Add(img_home);
+            left_main1.Controls.Add(btn_home);
+            resources.ApplyResources(left_main1, "left_main1");
+            left_main1.Name = "left_main1";
+            // 
+            // img_home
+            // 
+            img_home.BackgroundImage = Properties.Resources._3044146_bar_chart_increase_states_statistic_icon;
+            resources.ApplyResources(img_home, "img_home");
+            img_home.Name = "img_home";
+            img_home.TabStop = false;
+            // 
+            // btn_home
+            // 
+            resources.ApplyResources(btn_home, "btn_home");
+            btn_home.FlatAppearance.BorderSize = 0;
+            btn_home.Name = "btn_home";
+            btn_home.UseVisualStyleBackColor = true;
+            btn_home.Click += btn_home_Click;
+            // 
+            // left_up
+            // 
+            left_up.Controls.Add(img_title);
+            resources.ApplyResources(left_up, "left_up");
+            left_up.Name = "left_up";
+            // 
+            // img_title
+            // 
+            img_title.BackgroundImage = Properties.Resources._4575067_browser_connection_engine_internet_magnifier_icon;
+            resources.ApplyResources(img_title, "img_title");
+            img_title.Name = "img_title";
+            img_title.TabStop = false;
             // 
             // panel2
             // 
             panel2.BackColor = Color.FromArgb(244, 194, 227);
-            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(panel_background);
+            panel2.Controls.Add(main_up);
             resources.ApplyResources(panel2, "panel2");
             panel2.Name = "panel2";
             // 
-            // panel3
+            // panel_background
             // 
-            panel3.Controls.Add(panel4);
-            resources.ApplyResources(panel3, "panel3");
-            panel3.Name = "panel3";
+            resources.ApplyResources(panel_background, "panel_background");
+            panel_background.Name = "panel_background";
+            // 
+            // main_up
+            // 
+            main_up.Controls.Add(lbl_title);
+            main_up.Controls.Add(panel4);
+            resources.ApplyResources(main_up, "main_up");
+            main_up.Name = "main_up";
+            // 
+            // lbl_title
+            // 
+            resources.ApplyResources(lbl_title, "lbl_title");
+            lbl_title.Name = "lbl_title";
             // 
             // panel4
             // 
@@ -66,10 +210,13 @@
             // 
             // btnClose
             // 
-            btnClose.BackColor = Color.FromArgb(125, 182, 247);
+            btnClose.BackColor = Color.FromArgb(244, 194, 227);
+            btnClose.BackgroundImage = Properties.Resources._1564505_close_delete_exit_remove_icon;
             resources.ApplyResources(btnClose, "btnClose");
             btnClose.FlatAppearance.BorderColor = Color.FromArgb(125, 182, 247);
             btnClose.FlatAppearance.BorderSize = 0;
+            btnClose.FlatAppearance.MouseDownBackColor = Color.FromArgb(125, 182, 247);
+            btnClose.FlatAppearance.MouseOverBackColor = Color.FromArgb(125, 182, 247);
             btnClose.Name = "btnClose";
             btnClose.UseVisualStyleBackColor = false;
             btnClose.Click += btnClose_Click;
@@ -80,21 +227,48 @@
             AutoScaleMode = AutoScaleMode.Dpi;
             ControlBox = false;
             Controls.Add(panel2);
-            Controls.Add(panel1);
+            Controls.Add(left);
             FormBorderStyle = FormBorderStyle.None;
             Name = "HyperVC";
+            left.ResumeLayout(false);
+            left_main4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)img_setting).EndInit();
+            left_server.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)img_server).EndInit();
+            left_data.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            left_main1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)img_home).EndInit();
+            left_up.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)img_title).EndInit();
             panel2.ResumeLayout(false);
-            panel3.ResumeLayout(false);
+            main_up.ResumeLayout(false);
             panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private Panel panel1;
+        private Panel left;
         private Panel panel2;
-        private Panel panel3;
+        private Panel main_up;
         private Panel panel4;
         private Button btnClose;
+        private Panel left_main4;
+        private Panel left_server;
+        private Panel left_data;
+        private Panel left_main1;
+        private Panel left_up;
+        private Button btn_home;
+        private PictureBox img_home;
+        private PictureBox img_title;
+        private Panel panel_background;
+        private Label lbl_title;
+        private PictureBox img_setting;
+        private Button btn_setting;
+        private PictureBox img_server;
+        private Button btn_server;
+        private PictureBox pictureBox1;
+        private Button btn_data;
     }
 }
--- HyperVC.cs
+++ HyperVC.cs
@@ -7,14 +7,39 @@ namespace HyperApplication
             InitializeComponent();
         }
 
-        private void panelMenu_Paint(object sender, PaintEventArgs e)
+        private void Control_Add(Form form)
         {
-
+            panel_background.Controls.Clear();    //移除所有控件
+            form.TopLevel = false;      //设置为非顶级窗体
+            form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; //设置窗体为非边框样式
+            form.Dock = System.Windows.Forms.DockStyle.Fill;                  //设置样式是否填充整个panel
+            panel_background.Controls.Add(form);        //添加窗体
+            form.Show();                      //窗体运行
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void btn_home_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_server_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_setting_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_data_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
